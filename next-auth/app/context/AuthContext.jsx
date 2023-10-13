f683ddd898bc81b852b8d4d25a8ed1c41474cd12
neeraj-gs <gsneeraj05@gmail.com>
2023-10-13 20:36:52 +0530

    Did CLeint side Rendering of Session by creting a context of session provider  wrapped it around the children then creted a componetn adn renderd it onto the page

---
+++ next-auth/app/context/AuthContext.jsx
@@ -0,0 +1,12 @@
+'use client' //we mark the page if we use any hooksor providers
+
+import {SessionProvider} from 'next-auth/react'
+
+export default function Provider({children}){
+   return (
+      <SessionProvider>
+         {children}
+      </SessionProvider>
+   )
+}
+//nextjs docs recommed to do any provider inside a  sepreate route
--- next-auth/app/page.js
+++ next-auth/app/page.js
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "./api/auth/[...nextauth]/route"
-
+import User from "./components/User" 
+//user compeont occurs after a second of flash because it is client side rendered
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
@@ -9,6 +10,8 @@ export default async function Home() {
       <h1>Home</h1>
       <h1>Server Side Rendered</h1>
       <pre>{JSON.stringify(session)}</pre>
+      <h1>Client Side Renderd</h1>
+      <User /> 
     </section>
   )
 }
