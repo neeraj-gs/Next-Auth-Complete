1e7e3e19b257c56f454f6de0718735a5be5a2c30
neeraj-gs <gsneeraj05@gmail.com>
2023-10-13 20:07:01 +0530

    Fixed teh error of importing credentials provider and altered the import syntax

--- next-auth/app/api/auth/[...nextauth]/route.jsx
+++ next-auth/app/api/auth/[...nextauth]/route.jsx
@@ -3,7 +3,7 @@
 import NextAuth from "next-auth/next";
 import prisma from '../../../libs/prismadb';
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { CredentialsProvider } from "next-auth/providers";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
 
