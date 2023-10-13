792eeb299d52837ab474d1c8f6a236d005671f89
neeraj-gs <gsneeraj05@gmail.com>
2023-10-13 19:38:44 +0530

    Imported required libraries such as prisma , its adapter credentials and oauth providers

--- next-auth/app/api/auth/[...nextauth]/route.jsx
+++ next-auth/app/api/auth/[...nextauth]/route.jsx
@@ -1,1 +1,8 @@
 //main entry point of our next-auth apllication
+
+import NextAuth from "next-auth/next";
+import prisma from '../../../libs/prismadb';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { CredentialsProvider } from "next-auth/providers";
+import GoogleProvider from 'next-auth/providers/google'
+import GithubProvider from 'next-auth/providers/github'
