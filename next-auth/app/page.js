bd7beb7ba9cca9ad47dfcf36bc277ea37e629e5c
neeraj-gs <gsneeraj05@gmail.com>
2023-10-13 20:17:38 +0530

    Just added small code on how to server seide render the cookies or sesiontoken and access tehm and render them on the webpage

--- next-auth/app/page.js
+++ next-auth/app/page.js
@@ -1,8 +1,14 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "./api/auth/[...nextauth]/route"
 
-export default function Home() {
+
+export default async function Home() {
+  const session = await getServerSession(authOptions)
   return (
     <section>
       <h1>Home</h1>
+      <h1>Server Side Rendered</h1>
+      <pre>{JSON.stringify(session)}</pre>
     </section>
   )
 }
