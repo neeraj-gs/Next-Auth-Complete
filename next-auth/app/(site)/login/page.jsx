459fe0f8ef535c3d796c3f173d54553b9f496c84
neeraj-gs <gsneeraj05@gmail.com>
2023-10-16 11:47:09 +0530

    FIxed teh toast error on the login page

--- next-auth/app/(site)/login/page.jsx
+++ next-auth/app/(site)/login/page.jsx
@@ -18,7 +18,7 @@ export default function Login() {
       if(callback?.error){
         toast.error(callback.error)
       }
-      if(callback?.ok && callback?.error){
+      if(callback?.ok && !callback?.error){
           toast.success('Logged in Successfully')
       }
     })
