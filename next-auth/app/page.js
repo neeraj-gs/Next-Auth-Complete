54e5c7fd3968cfe89aec5bab866471f72599a045
neeraj-gs <gsneeraj05@gmail.com>
2023-10-16 09:03:53 +0530

    Checked the working of Conenction of CLient side and server side sesion login to next-auth

--- next-auth/app/page.js
+++ next-auth/app/page.js
@@ -1,7 +1,9 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "./api/auth/[...nextauth]/route"
-import User from "./components/User" 
+import User from "./components/User"
 //user compeont occurs after a second of flash because it is client side rendered
+//conenction of next-auth is checked using the cleint renderd or server seide renderd 
+
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
