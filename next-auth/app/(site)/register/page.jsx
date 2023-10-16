0b02dc2686363c937ec5397c7daf4ad3a05bde0f
neeraj-gs <gsneeraj05@gmail.com>
2023-10-16 09:44:37 +0530

    Added a on change and value for each form input for collecteing the form data

--- next-auth/app/(site)/register/page.jsx
+++ next-auth/app/(site)/register/page.jsx
@@ -1,4 +1,10 @@
-export default function Register() {
+'use client'
+
+import { useState } from 'react'
+
+export default function Register() { 
+
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
