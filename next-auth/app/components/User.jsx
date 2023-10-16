e6b852b743d2c704d1cc1be6a8758af5d293175c
neeraj-gs <gsneeraj05@gmail.com>
2023-10-16 11:26:20 +0530

    isntalled hot toast for better and profession alerts

--- next-auth/app/components/User.jsx
+++ next-auth/app/components/User.jsx
@@ -3,7 +3,7 @@
 import { useSession } from 'next-auth/react'
 
 const User = () => {
-   const {data:session} = useSession() //to use a session of login or a session conenction
+   const {data:session} = useSession()  //to use a session of login or a session conenction
   return (
     <div>
       {JSON.stringify(session)}
