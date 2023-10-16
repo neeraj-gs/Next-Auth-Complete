834ef4ae45afbd056408a25adc34d3c3ce67257a
neeraj-gs <gsneeraj05@gmail.com>
2023-10-16 09:55:33 +0530

    Added  ROute page for the backend api of register that checks and validate steh user and cretes a new user and adds to teh database

---
+++ next-auth/app/api/register/route.jsx
@@ -0,0 +1,33 @@
+import bcrypt from 'bcrypt';
+import { NextResponse } from 'next/server'; //allows us to send responses back to cleint
+import prisma from '../../libs/prismadb';
+
+export async function POST(request){ //we dont name functino in routes we only call usign the http methord
+   const body = request.json();
+   const {name,email,password} = body;
+
+   if(!name ||!email || !password){
+      return new NextResponse('MIssing Fields',{status:400})
+   }
+
+   const exist = await prisma.user.findUnique({ //find if already an email exists
+      where:{
+         email
+      }
+   });
+
+   if(exist){
+      return new NextResponse('Email Already Exists',{status:400})
+   }
+   
+   const hashedPassword = await bcrypt.hash(password,10); //hashing hte password before storing int het database
+   //salt is the strength of teh apssword  , here salt = 10 
+
+   const user = await prisma.user.create({
+      data:{
+         name,
+         email,
+         hashedPassword
+      }
+   })
+}
